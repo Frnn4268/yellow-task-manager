5ca45c84dd6e28f965f4a7eb4ceacc1df880d0ae
Frnn4268 <appstutorialesy@gmail.com>
2024-06-28 19:06:20 -0600

    Adding new component called Navbar.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,8 @@ import Users from './pages/Users'
 import Trash from './pages/Trash'
 import TaskDetails from './pages/TaskDetails'
 
-import Sidebar from './components/Sidbar'
+import Sidebar from './components/Sidebar'
+import Navbar from './components/Navbar'
 
 function Layout() {
   const { user } = useSelector((state) => state.auth)
@@ -24,7 +25,7 @@ function Layout() {
       {/* <MobileSidebar /> */}
 
       <div className='flex-1 overflow-y-auto'>
-        {/* <Navbar /> */}
+        <Navbar />
 
         <div className='p-4 2xl:px-10'>
           <Outlet />
