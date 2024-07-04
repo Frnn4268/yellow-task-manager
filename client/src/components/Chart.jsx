0d4c37c714cda7fd98623ecd154f10bd9d102bf8
Frnn4268 <appstutorialesy@gmail.com>
2024-07-04 17:16:31 -0600

    Adding new component called Chat.jsx in Dashboard

---
+++ client/src/components/Chart.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import {
+    Bar,
+    BarChart,
+    CartesianGrid,
+    Legend,
+    ResponsiveContainer,
+    Tooltip,
+    XAxis,
+    YAxis,
+  } from "recharts";
+import { chartData } from "../assets/data";
+
+const Chart = () => {
+  return (
+    <ResponsiveContainer 
+    width={'100%'}
+    height={500}
+    >
+      <BarChart
+      width={150}
+      height={40}
+      data={chartData}
+      >
+        <XAxis dataKey={'name'} />
+        <YAxis dataKey={'total'}/>
+        <Tooltip />
+        <Legend />
+        <CartesianGrid strokeDasharray='3 3' />
+        <Bar 
+        dataKey={'total'}
+        fill='#F7565C'
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  )
+}
+
+export default Chart
