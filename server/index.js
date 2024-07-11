48ef60f5603efa7d8ce3de789ad2c816a51b3029
Frnn4268 <appstutorialesy@gmail.com>
2024-07-11 16:47:25 -0600

    Adding cors config, express config and using cookieParser and morgan in dev mode

--- server/index.js
+++ server/index.js
@@ -6,6 +6,8 @@ import morgan from 'morgan'
 
 import dbConnection from './utils/index.js' // Database connection import from utils
  
+const routes = ''
+
 dotenv.config()
 
 dbConnection()
@@ -13,3 +15,18 @@ dbConnection()
 const PORT = process.env.PORT || 3001
 
 const app = express()
+
+app.use(
+    cors({
+        origin: ['http://localhost:3000', 'http://localhost:3001'],
+        methods: ['GET', 'POST', 'PUT', 'DELETE'],
+        credentials: true
+    })
+)
+
+app.use(express.json())
+app.use(express.urlencoded())
+
+app.use(cookieParser())
+app.use(morgan('dev'))
+app.use('/api', routes)
