141d511a55960fc1633284cb04b064cf1b317832
Frnn4268 <appstutorialesy@gmail.com>
2024-07-11 21:20:17 -0600

    Adding new util called createJWT.js and modifying the userRoutes.js

---
+++ server/utils/createJWT.js
@@ -0,0 +1,14 @@
+import jwt from "jsonwebtoken"
+
+export const createJWT = (res, userId) => {
+    const token = jwt.sign({ userId }, process.env.JWT_SECRET, { 
+        expiresIn: '7d' 
+    })
+
+    res.cookie('token', token, {
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== 'development' ,
+        sameSite: 'strict', // Prevent CSRF attacks
+        maxAge: 1 * 24 * 60 * 60 * 1000, // 1 day
+    })
+}
