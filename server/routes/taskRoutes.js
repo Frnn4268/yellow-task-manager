e0ef05193246ca0e39230ab923fe5b94973dc670
Frnn4268 <appstutorialesy@gmail.com>
2024-07-14 16:38:22 -0600

    Adding new controller called createSubTask and initializing another ones called updateTask and trashTask

--- server/routes/taskRoutes.js
+++ server/routes/taskRoutes.js
@@ -1,10 +1,14 @@
 import express from "express";
 import {
+  createSubTask,
   createTask,
   dashboardStatistics,
   duplicateTask,
+  getTask,
   getTasks,
   postTaskActivity,
+  trashTask,
+  updateTask,
 } from "../controllers/taskController.js";
 import { isAdminRoute, protectRoute } from "../middlewares/authMiddleware.js";
 
@@ -16,6 +20,17 @@ router.post("/activity/:id", protectRoute, postTaskActivity);
 
 router.get("/dashboard", protectRoute, dashboardStatistics);
 router.get("/", protectRoute, getTasks);
-// router.get("/:id", protectRoute, getTask);
+router.get("/:id", protectRoute, getTask);
+
+router.put("/create-subtask/:id", protectRoute, isAdminRoute, createSubTask);
+router.put("/update/:id", protectRoute, isAdminRoute, updateTask);
+router.put("/:id", protectRoute, isAdminRoute, trashTask);
+
+// router.delete(
+//   "/delete-restore/:id?",
+//   protectRoute,
+//   isAdminRoute,
+//   deleteRestoreTask
+// );
 
 export default router;
